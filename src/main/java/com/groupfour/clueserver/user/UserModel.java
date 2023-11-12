9cd613955f66a3760a1f46bd71b4a1aad809e5d0
JimmyVin <jimmy-dragon1@hotmail.com>
2023-11-12 11:52:49 -0500

    add set and get user

--- src/main/java/com/groupfour/clueserver/user/UserModel.java
+++ src/main/java/com/groupfour/clueserver/user/UserModel.java
@@ -2,7 +2,6 @@ package com.groupfour.clueserver.user;
 
 import com.groupfour.clueserver.rol.RolModel;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,13 +13,66 @@ import lombok.Data;
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Integer id;
+    private Integer userId;
     private String firstName;
     private String lastName;
     private RolModel roleId;
     private String email;
     private String password;
-    private Long numberPhone;
+    private String numberPhone;
     private byte profilePicture;
 
+        // =============METHODS==============
+    // =============GET===============
+    public Integer getUserId() {
+        return this.userId;
+    }
+    public String getUserFirstName() {
+        return this.firstName;
+    }
+    public String getUserLastName() {
+        return this.lastName;
+    }
+    public RolModel getRoleId() {
+        return this.roleId;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getNumberPhone() {
+        return this.numberPhone;
+    }
+    public byte getProfilePicture() {
+        return this.profilePicture;
+    }
+    // =========SET=======================
+    public void setUserId(Integer newId) {
+        this.userId=newId;
+    }
+    public void setUserFirstName(String newfirstName) {
+        this.firstName=newfirstName;
+    }
+    public void setUserLastName(String newLastName) {
+        this.lastName=newLastName;
+    }
+    public void setRoleId(RolModel newRoleId) {
+        this.roleId=newRoleId;
+    }
+    public void setEmail(String newEmail) {
+        this.email=newEmail;
+    }
+    public void setPassword(String newPassword) {
+        this.password=newPassword;
+    }
+    public void setNumberPhone(String newNumberPhone) {
+        this.numberPhone=newNumberPhone;
+    }
+    public void setProfilePicture(byte newProfilePicture) {
+        this.profilePicture=newProfilePicture;
+    }
+    //====================================
+    
 }
